48738c723518d0869e5e73d9315c08dec7e85dff
miao <miao@miao>
2023-11-18 13:42:30 -0800

    feat(server): add firebase config

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 import { initializeApp } from 'firebase/app'
 
-/**
- * TODO: Replace the following with your app's Firebase project configuration
- * 
- * @type {import('firebase/app').FirebaseOptions}
- */
+/** @type {import('firebase/app').FirebaseOptions} */
 const config = {
     // ...
+    apiKey: 'AIzaSyBuyxUIkvRzk8zBzEzjgRc8nn19_PGQtC4',
+    authDomain: 'misha-23f13.firebaseapp.com',
+    projectId: 'misha-23f13',
+    storageBucket: 'misha-23f13.appspot.com',
+    messagingSenderId: '499886641911',
+    appId: '1:499886641911:web:eb32d94a281222fbccb9f2',
+    measurementId: 'G-FB3SB5D7H7',
 }
 
 export const app = initializeApp(config)
