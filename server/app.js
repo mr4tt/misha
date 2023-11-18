cca0a4e4816901e8ac9231f065490578bf261eed
miao <miao@miao>
2023-11-18 13:22:26 -0800

    feat(server): add firebase boilerplate

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+/**
+ * TODO: Replace the following with your app's Firebase project configuration
+ * 
+ * @type {import('firebase/app').FirebaseOptions}
+ */
+const config = {
+    // ...
+}
+
+export const app = initializeApp(config)
+
