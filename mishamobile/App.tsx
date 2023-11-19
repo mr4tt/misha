8356cb289ee2464068a1c4247b3f1fcc3e9aa246
Mrat <78581772+mr4tt@users.noreply.github.com>
2023-11-18 16:38:18 -0800

    adding dependencies for nav

--- mishamobile/App.tsx
+++ mishamobile/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+
 import {ScanScreen} from './src/Scan';
 import {ChooseScreen} from './src/Choose';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {TagEvent} from 'react-native-nfc-manager';
 
 export type RootStackParamList = {
@@ -16,11 +18,13 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ScanScreen" component={ScanScreen} />
-        <Stack.Screen name="ChooseScreen" component={ChooseScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ScanScreen" component={ScanScreen} />
+          <Stack.Screen name="ChooseScreen" component={ChooseScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
